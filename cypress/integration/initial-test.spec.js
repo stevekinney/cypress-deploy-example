36ef6bb76a8abceea2fab930c8f2cfdabc1734dd
Steve Kinney <hello@stevekinney.net>
2021-12-12 09:06:22 -0700

    Update initial-test.spec.js

--- cypress/integration/initial-test.spec.js
+++ cypress/integration/initial-test.spec.js
@@ -1,6 +1,6 @@
 describe('Create React App', () => {
   it('should contain a link to learning React', () => {
     cy.visit('http://localhost:3000/');
-    cy.contains('Learn Svelte');
+    cy.contains('Learn Cypress');
   });
 });
